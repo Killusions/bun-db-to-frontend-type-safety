d42aa8775ebfe5d08d179846e066f1af5df6e40f
Linus Schlumberger <linus.schlumberger@siemens.com>
2025-08-04 13:16:03 +0200

    feat: add tailwindcss

--- frontend/dev/build.ts
+++ frontend/dev/build.ts
@@ -1,8 +1,9 @@
+import TailwindPlugin from 'bun-plugin-tailwind';
 import VuePlugin from '../plugin/vue-plugin';
 
 await Bun.build({
   entrypoints: ['frontend/index.html'],
   outdir: 'dist/frontend',
-  plugins: [VuePlugin],
+  plugins: [VuePlugin, TailwindPlugin],
   env: "inline"
 });
